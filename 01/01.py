53e496d0ab459cff28de59b7e57a7c879fef5577
Juraj Trappl <juraj.trappl@proton.me>
2023-12-01 13:04:30 +0100

    add solution to the first day

---
+++ 01/01.py
@@ -0,0 +1,10 @@
+import regex as re
+
+with open("input", "r") as f:
+    input = f.read()
+
+def solve(digits):
+    return sum(int(digits[candidates[0]] + digits[candidates[-1]]) for candidates in [re.findall(r"|".join(digits), l, overlapped=True) for l in input.splitlines()])
+
+print(solve({str(d): str(d) for d in range(1, 10)})) # 1-9
+print(solve({str(d): str(d) for d in range(1, 10)} | {d: str(i+1) for i, d in enumerate(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"])})) # + digits using letters
