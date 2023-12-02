b12d02116cbdbc76453e92cfec0fa6cb527f4c50
Juraj Trappl <juraj.trappl@proton.me>
2023-12-02 09:33:32 +0100

    rename to more meaningful name, add part solution comments

--- 02/02.py
+++ 02/02.py
@@ -11,11 +11,11 @@ with open("02.in", "r") as f:
     ]
 
 bag = {"red": 12, "green": 13, "blue": 14}
-possible_games_ids = set([id for id, _ in games])
-min_set_cubes = 0
+possible_games_ids = set([id for id, _ in games])   # part 1
+power = 0                                           # part 2
 
 for id, record in games:
-    max_bag = {"red": 0, "green": 0, "blue": 0}
+    fewest_cubes_bag = {"red": 0, "green": 0, "blue": 0}
     for r in record:
         record_bag = {
             color: int(count)
@@ -24,11 +24,11 @@ for id, record in games:
         if any(bag[color] - record_bag.get(color, 0) < 0 for color in bag):
             possible_games_ids.discard(id)
 
-        max_bag = {
-            color: max(max_bag[color], record_bag.get(color, 0)) for color in bag.keys()
+        fewest_cubes_bag = {
+            color: max(fewest_cubes_bag[color], record_bag.get(color, 0)) for color in bag
         }
 
-    min_set_cubes += reduce(lambda x, y: x * y, max_bag.values())
+    power += reduce(lambda x, y: x * y, fewest_cubes_bag.values())
 
 print(sum(possible_games_ids))
-print(min_set_cubes)
+print(power)
