c9e0307159c71870a433339923be6ee2566e9ff3
Juraj Trappl <juraj.trappl@proton.me>
2023-12-02 23:59:20 +0100

    improve the solution

--- 02/02.py
+++ 02/02.py
@@ -1,33 +1,33 @@
 import regex as re
 
+
+def parse_record(record):
+    cubes = re.findall(r"(\d+)\s+(\w+)", record)
+    return {color: int(count) for count, color in cubes}
+
+
 with open("02.in", "r") as f:
     games = [
-        (int(id), record.split(";"))
-        for id, record in [
+        (int(id), [parse_record(record) for record in records.split(";")])
+        for id, records in [
             re.match(r"Game (\d+): (.*)", line).groups()
             for line in f.read().strip().splitlines()
         ]
     ]
 
 bag = {"red": 12, "green": 13, "blue": 14}
-possible_games_ids = set(id for id, _ in games)   # part 1
-power = 0                                           # part 2
+possible_games_ids = set(id for id, _ in games)
+power = 0
+for id, records_bags in games:
+    max_cubes_bag = {
+        color: max(record_bag.get(color, 0) for record_bag in records_bags)
+        for color in bag
+    }
 
-for id, record in games:
-    fewest_cubes_bag = {"red": 0, "green": 0, "blue": 0}
-    for r in record:
-        record_bag = {
-            color: int(count)
-            for count, color in (cube.split() for cube in r.split(","))
-        }
-        if any(bag[color] - record_bag.get(color, 0) < 0 for color in bag):
-            possible_games_ids.discard(id)
+    if any(bag[color] - max_cubes_bag[color] < 0 for color in bag):
+        possible_games_ids.discard(id)
 
-        fewest_cubes_bag = {
-            color: max(fewest_cubes_bag[color], record_bag.get(color, 0)) for color in bag
-        }
-
-    power += fewest_cubes_bag["red"] * fewest_cubes_bag["green"] * fewest_cubes_bag["blue"]
+    power += max_cubes_bag["red"] * max_cubes_bag["green"] * max_cubes_bag["blue"]
 
 print(sum(possible_games_ids))
 print(power)
