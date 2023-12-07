aabc4f5d68ca1f9067dc2c6c689f774c4960b00a
Juraj Trappl <juraj.trappl@proton.me>
2023-12-07 21:55:40 +0100

    add solution to 7.2 + formatted

--- day_07.py
+++ day_07.py
@@ -1,21 +1,56 @@
-from collections import Counter
+"""
+--- Day 7: Camel Cards ---
+"""
+from collections import Counter, defaultdict
+from typing import Dict, List, Tuple
 
-with open("test.in", "r", encoding="utf-8") as f:
-    hands_with_bids = [line.split() for line in f.read().strip().splitlines()]
-
-camel_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-types = {
-    t: []
-    for t in [(5,), (4, 1), (3, 2), (3, 1, 1), (2, 2, 1), (2, 1, 1, 1), (1, 1, 1, 1, 1)]
+p1_camel_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+p2_camel_cards = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
+type_frequencies = {
+    "FIVE_OF_A_KIND": (5,),
+    "FOUR_OF_A_KIND": (4, 1),
+    "FULL_HOUSE": (3, 2),
+    "THREE_OF_A_KIND": (3, 1, 1),
+    "TWO_PAIR": (2, 2, 1),
+    "ONE_PAIR": (2, 1, 1, 1),
+    "HIGH_CARD": (1, 1, 1, 1, 1),
 }
 
-for hand, bid in hands_with_bids:
-    freq = tuple([f[1] for f in Counter(hand).most_common()])
-    types[freq].append(([camel_cards.index(card) for card in hand], int(bid)))
+def cards_to_relative_strength(cards: List[str], hand: str) -> List[int]:
+    return [cards.index(card_on_hand) for card_on_hand in hand]
 
-hand_counter, total = 1, 0
-for t, hands in reversed(types.items()):
-    ranked_hands = sorted(hands, key=lambda x: x[0])
-    for hand, bid in ranked_hands:
-        total += hand_counter * bid
-        hand_counter += 1
+def count_score(types: Dict[Tuple[int, ...], List[int]]) -> int:
+    hand_counter, total = 1, 0
+    for hands in reversed(types.values()):
+        ranked_hands = sorted(hands, key=lambda x: x[0])
+        for _, bid in ranked_hands:
+            total += hand_counter * bid
+            hand_counter += 1
+
+    return total
+
+with open("07.in", "r", encoding="utf-8") as f:
+    hands_with_bids = [line.split() for line in f.read().strip().splitlines()]
+
+p1, p2 = defaultdict(list), defaultdict(list)
+for hand, str_bid in hands_with_bids:
+    hand_counts = tuple(f[1] for f in Counter(hand).most_common())
+    p1[hand_counts].append(
+        (cards_to_relative_strength(p1_camel_cards, hand), int(str_bid))
+    )
+
+    hand_without_joker = Counter(hand.replace("J", "")).most_common()
+    if not hand_without_joker:
+        p2[type_frequencies["FIVE_OF_A_KIND"]].append(
+            (cards_to_relative_strength(p2_camel_cards, hand), int(str_bid))
+        )
+        continue
+
+    most_frequent_card_without_joker = hand_without_joker[0][0]
+    joker_hand = hand.replace("J", most_frequent_card_without_joker)
+    joker_hand_counts = tuple(f[1] for f in Counter(joker_hand).most_common())
+    p2[joker_hand_counts].append(
+        (cards_to_relative_strength(p2_camel_cards, hand), int(str_bid))
+    )
+
+print(count_score(p1), count_score(p2))
